160bbdfbe16de09e8a6a33ef40501c60a08602fb
Moamin <moamin.salamah@gmail.com>
2023-03-08 13:46:09 +0100

    home section

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ MenuBtn.addEventListener('click',function(e){
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark');
 });
+
+// typing effect
+let typed = new Typed('.auto-input',{
+    strings:['Gard Dog','Sleeper','Dancer','Make you smile'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay:2000,
+    loop: true,
+});
